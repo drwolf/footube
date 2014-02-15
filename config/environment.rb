2508e2de895f50e1e66b1c54682bba0d3e67f481
Klaus Meyer <spam@klaus-meyer.net>
2014-02-15 21:25:40 +0100

    fix mailing from production site

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Footube::Application.initialize!
+
+# don't validate the ssl certificate of the local mailserver
+ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
