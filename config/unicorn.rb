f3c6f02d44f22e50b8780f44f62419ae52a3872f
Klaus Meyer <spam@klaus-meyer.net>
2014-04-07 21:43:32 +0200

    modify unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -51,7 +51,7 @@ after_fork do |server, worker|
 
   begin
     uid, gid = Process.euid, Process.egid
-    user, group = 'www', 'www-data'
+    user, group = 'www', 'www'
     target_uid = Etc.getpwnam(user).uid
     target_gid = Etc.getgrnam(group).gid
     worker.tmp.chown(target_uid, target_gid)
